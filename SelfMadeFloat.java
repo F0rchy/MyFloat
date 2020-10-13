0ac784c6880341dd175a2f8652cca55c6e0f9d8b
Artem <theartemka6@mail.ru>
2020-10-13 19:39:30 +0300

    Added fields and Constructor

---
+++ SelfMadeFloat.java
@@ -0,0 +1,14 @@
+package com.max.idea;
+
+public class SelfMadeFloat
+{
+    private long mantissa;
+    private long exponent;
+    private long myInteger;
+    private long myFraction;
+
+    public SelfMadeFloat(long mantissa, long exponent) {
+        this.mantissa = mantissa;
+        this.exponent = exponent;
+    }
+}
