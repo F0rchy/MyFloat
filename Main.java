988ef44efd85878332f4222d6be0a5e129e2ce25
Artem <theartemka6@mail.ru>
2020-10-13 19:33:44 +0300

    Added Main

---
+++ Main.java
@@ -0,0 +1,10 @@
+package com.max.idea;
+
+public class Main {
+    public static void main(String[] args) {
+    SelfMadeFloat a = new SelfMadeFloat(23456001,3);
+    SelfMadeFloat b = new SelfMadeFloat(86487010,3);
+    a.sum(b);
+    a.dif(b);
+    }
+}
